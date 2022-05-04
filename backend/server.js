5112f2ef31159d4dd29b96dbe0474e54fcff3b87
SnrMatt <msmreyes551@gmail.com>
2022-05-04 06:31:50 -0700

    Implemented Better Dev Env

--- backend/server.js
+++ backend/server.js
@@ -1,117 +1,103 @@
 const express = require('express');
-const app = express();
+const app  = express();
+const cors = require('cors')
 const server = require('http').createServer(app);
-const {
-    Server
-} = require('socket.io');
-const io = new Server(server);
-app.use(express.static('build'));
+const { Server } = require('socket.io');
+const io = new Server(server,{
+  cors:{
+    origin:'http://localhost:3000'
+  }
+});
 
+
+
+app.use(cors());
 var current_list = [];
 let lobbies = {};
-io.on('connection', (socket) => {
+io.on('connection', (socket)=>{
     current_list.push(socket.id)
     //#Handle Room creation, Joining a room, giving room information to users.
-    socket.on('request_room', (username) => {
-        var id = generateID();
-        let set_leader = {
-            username: username,
-            role: 'leader',
-            points: 0,
-            background: `rgb(${Math.floor( Math.random()*256/2)},${Math.floor(Math.random()*256/2)},${Math.floor(Math.random()*256/2)})`
-        }
-        lobbies[id] = {
-            users: [set_leader],
-            sockets: [socket.id],
-            currentUserTurn: 0
-        }
-
-        socket.emit('roomID', {
-            id: id
-        });
+    socket.on('request_room', (username)=>{
+      var id = generateID();
+      let set_leader = 
+      {
+        username:username,
+        role: 'leader',
+        points: 0,
+        background: `rgb(${Math.floor( Math.random()*256/2)},${Math.floor(Math.random()*256/2)},${Math.floor(Math.random()*256/2)})`
+      }
+      lobbies[id] = {
+        users: [set_leader],
+        sockets: [socket.id],
+        currentUserTurn : 0
+      }
+   
+      socket.emit('roomID', {id:id});
     })
 
+   
 
-
-    socket.on('validate_room', ({
-        id
-    }, username) => {
-        list = Array.from(io.sockets.adapter.rooms);
-        list = list.filter(room => !room[1].has(room[0]))
-        list = list.map(rooms => rooms[0])
-
-        let found = list.indexOf(id)
-        if (found != -1 || lobbies.length < 6) {
-            lobbies[id]['users'].push({
-                username: username,
-                points: 0,
-                role: 'player',
-                background: `rgb(${Math.floor( Math.random()*256/2)},${Math.floor(Math.random()*256/2)},${Math.floor(Math.random()*256/2)})`,
-            })
-            lobbies[id]['sockets'].push(socket.id)
-        } else {
-            found = -1;
-        }
-        socket.emit('validation_response', found, {
-            id: id
-        });
+    socket.on('validate_room', ({id}, username)=>{
+      list = Array.from(io.sockets.adapter.rooms);
+      list = list.filter(room => !room[1].has(room[0]))
+      list = list.map(rooms=>rooms[0])
+      
+      let found  = list.indexOf(id)
+      if(found != -1 || lobbies.length < 6){
+        lobbies[id]['users'].push({
+          username:username,
+          points:0,
+          role:'player',
+          background: `rgb(${Math.floor( Math.random()*256/2)},${Math.floor(Math.random()*256/2)},${Math.floor(Math.random()*256/2)})`,
+        })
+        lobbies[id]['sockets'].push(socket.id)
+      }
+      else {
+        found = -1;
+      }
+      socket.emit('validation_response', found,{id: id});
     })
+    
 
 
-
-    socket.on('join_room', ({
-        id
-    }) => {
-        id = id.slice(1);
-        let currentUserIndex = lobbies[id]['users'].length - 1;
-        console.log(currentUserIndex);
-        socket.join(id);
-        io.to(id).emit('new_user', lobbies[id].users, lobbies[id]['users'][currentUserIndex]);
-
+    socket.on('join_room', ({id})=>{
+      id = id.slice(1);
+      let currentUserIndex =  lobbies[id]['users'].length - 1;
+      socket.join(id);
+      socket.emit('get_user', lobbies[id]['users'][currentUserIndex])
+      io.to(id).emit('new_user', lobbies[id].users );
     })
     //////////////////////////////////////////////////
     //#Handle gameplay start session
+  
 
-
-    //#Handle Drawing
-    socket.on('position', ({
-        x,
-        y,
-        x2,
-        y2
-    }, {
-        id
-    }) => {
-        id = id.slice(1);
-        io.to(id).emit('draw', {
-            x,
-            y,
-            x2,
-            y2
-        })
+//#Handle Drawing
+    socket.on('position',({x,y,x2,y2}, {id})=>{
+      id = id.slice(1);
+      io.to(id).emit('draw', {x,y,x2,y2})
     })
-    //#Handling Chat message inside the lobby
-    socket.on('send_chat', (username, message, {
-        id
-    }) => {
-        id = id.slice(1);
-        socket.to(id).emit('new_message', username, message)
-    })
+//#Handling Chat message inside the lobby
+  socket.on('send_chat', (username, message, {id})=>{
+    id = id.slice(1); 
+    socket.to(id).emit('new_message', username, message)
+  })
 
-    socket.on("disconnect", () => {
-        let user = current_list.indexOf(socket.id);
-        current_list.splice(user, 1);
-    })
+  socket.on("disconnect", ()=>{
+    let user = current_list.indexOf(socket.id);
+    current_list.splice(user, 1);
+  })
 
-})
+  })  
 
 
 
 
-server.listen(80, () => {
-    console.log('Server is on http://localhost')
+
+
+server.listen(4001, ()=>{
+    console.log('Server is on *4001')
 })
 
 function generateID() {
-    return Math.floor((1 + Math.random()) * 0x1000).toString(16).substring();
+  return Math.floor((1+Math.random()) * 0x1000).toString(16).substring();
 }
--- frontend/src/Pages/CanvasBoard.js
+++ frontend/src/Pages/CanvasBoard.js
@@ -1,7 +1,6 @@
 import { useContext, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { SocketContext } from "../Utils/Socketprovider";
-import UserDisplay from "../Components/UserDisplay";
 import UserProfile from "../Components/UserProfile";
 import Self_Message from "../Components/Messages/self_message";
 import Others_Message from "../Components/Messages/others_message";
@@ -22,71 +21,95 @@ export default function Canvasboard(){
     
 
     useEffect(()=>{
-        
+        function gameSetup(ctx,canvas, healthCtx,healthbar) {
+            ctx.fillStyle='white';
+            ctx.fillRect(0,0,canvas.width, canvas.height);
+           // healthCtx.fillStyle = 'red';
+          //  healthCtx.fillRect(0,0,healthbar.width, healthbar.height);
+           // healthCtx.fillStyle = '#22BF7B';
+           // healthCtx.fillRect(0,0, healthbar.width, healthbar.height);
+    }
+    
+    
 
         socket.emit('join_room', roomID);
-        socket.on('new_user', (lobby_info, currentUser)=>{
+        socket.on('new_user', (lobby_info)=>{
             console.log(lobby_info);
                 setLobby(lobby_info);
-                setUser(currentUser);
+        })
+
+        socket.on('get_user', (currentUser)=>{
+            console.log('client got user name');
+            setUser(currentUser);
         })
         socket.on('get_role', (user_role)=>{
             setRole(user_role);
         })
-        let canvas = canvasRef.current, healthbar = healthBarRef.current;
-        let ctx = canvas.getContext('2d'), healthCtx = healthbar.getContext('2d');
-        gameSetup(ctx, healthCtx,canvas,healthbar);
+       let canvas = canvasRef.current
+       let ctx = canvas.getContext('2d');
+ 
+       window.addEventListener('resize', ()=>{
+           canvas.width = window.innerWidth;
+           canvas.height = window.innerHeight;
+           ctx.fillStyle='white';
+           ctx.fillRect(0,0,canvas.width, canvas.height);
+   
+       })
+
+        //let healthbar = healthBarRef.current;
+        //let healthCtx = healthbar.getContext('2d');
+        gameSetup(ctx, canvas);
         
-        // const handleHealthBar = (health) =>{
-        //     healthCtx.fillStyle = 'red';
-        // healthCtx.fillRect(0,0,healthbar.width, healthbar.height);
-        // healthCtx.fillStyle = '#22BF7B';
-        // healthCtx.fillRect(0,0,  healthbar.width - (healthbar.width * health), healthbar.height);
-        // }
-        // canvas.addEventListener('mousemove', (e)=>{
-        //     x = e.offsetX;
-        //     y = e.offsetY;
-        // })
-        // canvas.addEventListener('mouseleave', ()=>{
-        //     clearInterval(interval)
-        // })
+        const handleHealthBar = (health) =>{
+       //     healthCtx.fillStyle = 'red';
+       // healthCtx.fillRect(0,0,healthbar.width, healthbar.height);
+        //healthCtx.fillStyle = '#22BF7B';
+        //healthCtx.fillRect(0,0,  healthbar.width - (healthbar.width * health), healthbar.height);
+        }
+        canvas.addEventListener('mousemove', (e)=>{
+            x = e.offsetX;
+            y = e.offsetY;
+        })
+        canvas.addEventListener('mouseleave', ()=>{
+            clearInterval(interval)
+        })
 
-        // canvas.addEventListener('mousedown', ()=>{
-        //     lastX = x;
-        //     lastY = y;
-        //     interval = setInterval(()=>{
-        //       if(total_time_drawn !== total_allowed_time){
-        //         socket.emit('position', {x: x, y: y, x2: lastX, y2 :lastY}, roomID)
-        //         if(x !== lastX  || y !== lastY){
-        //             total_time_drawn++;
-        //             total_health = total_time_drawn/total_allowed_time;
-        //             window.requestAnimationFrame(handleHealthBar(total_health));
-        //         }
+        canvas.addEventListener('mousedown', ()=>{
+            lastX = x;
+            lastY = y;
+            interval = setInterval(()=>{
+              if(total_time_drawn !== total_allowed_time){
+                socket.emit('position', {x: x, y: y, x2: lastX, y2 :lastY}, roomID)
+                if(x !== lastX  || y !== lastY){
+                    total_time_drawn++;
+                    total_health = total_time_drawn/total_allowed_time;
+                    window.requestAnimationFrame(handleHealthBar(total_health));
+                }
                     
-        //       }
-        //       else {
-        //         clearInterval(interval)
-        //       }
-        //     }, 0)
+              }
+              else {
+                clearInterval(interval)
+              }
+            }, 0)
 
 
-        // })
-        // canvas.addEventListener('mouseup', ()=>{
-        //     clearInterval(interval);
-        // })
-        // socket.on('draw',({x,y,x2,y2})=>{
-        //     ctx.fillStyle='black';
-        //    if(lastX !== x || lastY !== y){
-        //     ctx.beginPath();
-        //     ctx.moveTo(x2, y2);
-        //     ctx.lineWidth =5;
-        //     ctx.lineCap = 'round';
-        //     ctx.lineTo(x,y)
-        //     ctx.stroke();
-        //    }
-        //    lastX = x
-        //    lastY = y;
-        // }) 
+        })
+        canvas.addEventListener('mouseup', ()=>{
+            clearInterval(interval);
+        })
+        socket.on('draw',({x,y,x2,y2})=>{
+            ctx.fillStyle='black';
+           if(lastX !== x || lastY !== y){
+            ctx.beginPath();
+            ctx.moveTo(x2, y2);
+            ctx.lineWidth =5;
+            ctx.lineCap = 'round';
+            ctx.lineTo(x,y)
+            ctx.stroke();
+           }
+           lastX = x
+           lastY = y;
+        }) 
 
         //#Chat Message Listeners
         socket.on('new_message', (username, message)=>{
@@ -96,58 +119,12 @@ export default function Canvasboard(){
     },[])
     return(
         <>
-        <div className="flex">
-            <div className="w-1/3 bg-gray-700 max-h-screen flex flex-col ">
-                <div className="h-5/6 flex flex-col justify-center">
-                    <div className="h-5/6 overflow-hidden overflow-y-auto flex flex-col gap-5 border-r-2 border-gray-600">
+            <div className ="bg-red-500 h-screen w-screen">
+                <canvas height ={window.innerHeight} width={window.innerWidth} ref={canvasRef}></canvas>
 
-                        {chatMessages}
-
-                    </div>
-                </div>
-                
-                
-                <div className="h-1/6 ">
-                    <input
-                    onKeyUp={(e)=>{
-                        if(e.key === 'Enter'){
-                        socket.emit('send_chat', user.username, e.target.value, roomID)
-                        addMessages([...chatMessages, <Self_Message username = {user.username}>{e.target.value}</Self_Message>])
-                        }
-                }}
-                    className="bg-gray-500 p-2 pl-3 rounded-md  w-4/6 block mx-auto mt-10 text-gray-400 focus:outline-none focus:text-white" 
-                    type='text'
-                    placeholder="Enter text.... "/>
-                </div>
             </div>
-
-
-
-
-            <div className="flex flex-col gap-5 justify-center items-center bg-gray-700 h-screen w-screen">
-                <div className=" flex px-10 h-24 w-1000 gap-4  justify-center  ">
-                    {lobby && lobby.map(user=>{return <UserProfile role = {user.role} background={user.background}>{user.username}</UserProfile>})}
-  
-                </div>
-                <div className="relative">
-                    <canvas className="rounded-md shadow-2xl" width={1000} height = {500} ref = {canvasRef}/>
-                <div className="block z-10 top-4 absolute left-1/2 -translate-x-1/2">
-                    <canvas className="rounded-xl shadow-2xl" ref ={healthBarRef} width = {200} height= {40}></canvas>
-                </div>
-                </div>
-            </div>
-        </div>
         </>
     );
 }
 
-
-function gameSetup(ctx, healthCtx,canvas,healthbar) {
-        ctx.fillStyle='white';
-        ctx.fillRect(0,0,canvas.width, canvas.height);
-        healthCtx.fillStyle = 'red';
-        healthCtx.fillRect(0,0,healthbar.width, healthbar.height);
-        healthCtx.fillStyle = '#22BF7B';
-        healthCtx.fillRect(0,0, healthbar.width, healthbar.height);
-}
 
